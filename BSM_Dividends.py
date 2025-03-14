e695a48c3c4cb4513379ea3a550656edc095e65b
dav <davidmarco1312@gmail.com>
2025-03-14 23:18:17 +0100

    Update BSM_Dividends.py

--- BSM_Dividends.py
+++ BSM_Dividends.py
@@ -108,3 +108,26 @@ axes[1].set_ylabel("Volatility")
 plt.tight_layout()
 st.pyplot(fig, clear_figure=True)
 plt.close(fig)
+
+# Tabla de Arriba y cosas esteticas
+st.subheader("📌 Model Inputs")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.metric(label="Current Asset Price", value=f"${current_price:.2f}")
+
+with col2:
+    st.metric(label="Strike Price", value=f"${strike:.2f}")
+
+with col3:
+    st.metric(label="Time to Maturity", value=f"{time_to_maturity:.2f} years")
+
+col4, col5 = st.columns(2)
+
+with col4:
+    st.metric(label="Volatility (σ)", value=f"{volatility:.2%}")
+
+with col5:
+   
+
